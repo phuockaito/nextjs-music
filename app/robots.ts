42b05c6771d6472d14a7d7c0dc6f3aaad5545aaa
phuockaito <huuphuocit1999@gmail.com>
2025-11-26 09:26:22 +0700

    build

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://nextjs-music.vercel.app/'
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://nextjs-music.vercel.app'
 
     return {
         rules: [
